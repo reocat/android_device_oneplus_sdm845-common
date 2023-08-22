b4890c1c17163ee16cb18527ebfce0bf7ae377eb
Edwin Moquete <edwinmmoquete@gmail.com>
2023-08-22 16:48:43 +0000

    sdm845-common: Remove unnecessary audio targets
    Change-Id: Iae2dc61a10373b3aa365cedddfec93d0fa08b7e5

--- common.mk
+++ common.mk
@@ -102,7 +102,6 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libbatterylistener \
-    libcirrusspkrprot \
     libcomprcapture \
     libexthwplugin \
     libhdmiedid \
@@ -113,10 +112,7 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libssrec \
-    libsynth \
-    libtinycompress \
     libvolumelistener \
-    tinymix \
     OnePlusDiracGef
 
 PRODUCT_COPY_FILES += \
