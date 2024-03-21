ec3b53259179d6b92aabf879f1948530650d68eb
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    sdm845-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- common.mk
+++ common.mk
@@ -455,7 +455,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vibrator
 PRODUCT_PACKAGES += \
