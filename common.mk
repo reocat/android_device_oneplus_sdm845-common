8367959421c6fc26ca8d9ba486df6d8f4d649a13
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-05-24 22:13:28 -0700

    sdm845-common: power: Set up cgroups & task_profiles
    Precedence goes /system/etc/cgroups.json|task_profiles.json,
    then API level versions in same dir, then vendor/etc/ versions
    (in increasing order; overriding individual entries within the
    json structure, rather than at file-level).
    
    We want to override any potentially-breaking changes or
    optimizations introduced by overzealous application of things
    beyond LineageOS-tested additions in system/core/.
    
    Copied cgroups_28.json and task_profiles_28.json from LineageOS
    android_system_core @ lineage-21.0 branch, then added entries
    for things modified in crDroid cgroups & task_profiles to put
    them back the way Lineage had it.

--- common.mk
+++ common.mk
@@ -379,7 +379,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # QMI
 PRODUCT_PACKAGES += \
